96b3d9c9d6ee82e49ada60177a948075d48d60b9
sokumon <sohamkulkarns9@gmail.com>
2025-05-22 12:14:39 +0530

    fix: handle imap abort in email server
    (cherry picked from commit 3c41ec9e435091391b7d874341baa4acb139ac7b)

--- frappe/email/receive.py
+++ frappe/email/receive.py
@@ -176,7 +176,7 @@ class EmailServer:
 
 		for i, uid in enumerate(email_list[:100]):
 			try:
-				self.retrieve_message(uid, i + 1)
+				self.retrieve_message(uid, i + 1, folder)
 			except (_socket.timeout, LoginLimitExceeded):
 				# get whatever messages were retrieved
 				break
@@ -255,7 +255,7 @@ class EmailServer:
 		else:
 			return None
 
-	def retrieve_message(self, uid, msg_num):
+	def retrieve_message(self, uid, msg_num, folder):
 		try:
 			if cint(self.settings.use_imap):
 				status, message = self.imap.uid("fetch", uid, "(BODY.PEEK[] BODY.PEEK[HEADER] FLAGS)")
@@ -269,7 +269,9 @@ class EmailServer:
 		except _socket.timeout:
 			# propagate this error to break the loop
 			raise
-
+		except imaplib.IMAP4.abort:
+			self.connect()
+			self.get_messages(folder)
 		except Exception as e:
 			if self.has_login_limit_exceeded(e):
 				raise LoginLimitExceeded(e)
