5dc5fb7667500b4b4ad87baa9663b627493a864b
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-07-19 11:59:10 +0530

    feat: auto append email account to the document
    (cherry picked from commit daf8f5545820a26b114102dded3733b2c777ae86)
    
    # Conflicts:
    #	frappe/core/doctype/doctype/doctype.json

--- frappe/core/doctype/doctype/doctype.py
+++ frappe/core/doctype/doctype/doctype.py
@@ -162,6 +162,7 @@ class DocType(Document):
 		route: DF.Data | None
 		row_format: DF.Literal["Dynamic", "Compressed"]
 		search_fields: DF.Data | None
+		sender_email_account_field: DF.Data | None
 		sender_field: DF.Data | None
 		sender_name_field: DF.Data | None
 		show_name_in_global_search: DF.Check
