000fe14b9d8f3c4510020af6c009cbc890d7b8eb
Akhil Narang <me@akhilnarang.dev>
2025-09-03 13:43:09 +0530

    chore: resolve conflicts

--- frappe/core/doctype/system_settings/system_settings.py
+++ frappe/core/doctype/system_settings/system_settings.py
@@ -72,15 +72,7 @@ class SystemSettings(Document):
 		max_file_size: DF.Int
 		minimum_password_score: DF.Literal["2", "3", "4"]
 		max_report_rows: DF.Int
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-		max_signups_per_hour: DF.Int
-=======
 		max_signups_allowed_per_hour: DF.Int
->>>>>>> c4c151ed9b (chore: update field name)
-		minimum_password_score: DF.Literal["1", "2", "3", "4"]
->>>>>>> 4829b50a8b (feat: allow users to define max signups per hour)
 		number_format: DF.Literal[
 			"#,###.##",
 			"#.###,##",
